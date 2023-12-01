3c17ec120393528d0f1ef410c325ad5a3e6e8ae0
Gfuss <gfuss@gfuss.net>
2023-12-01 18:28:20 -0500

    Day1Final

--- day1/trebuchet.py
+++ day1/trebuchet.py
@@ -1,24 +1,33 @@
+count = 0
+lineNum = 0
 
-elves = []
-calCount = 0
 inputFile = open( 'input.txt', 'r')
 
 lines = inputFile.readlines()
 
 
 for line in lines:
-    if line.strip():
-       calCount += int(line.strip())
-    else:
-        elves.append( calCount )
-        calCount = 0
+    numbers = []
+    origLine = line
+    #this is gross, but for part 2, find 'words' that are numbers and replace the.  Words truncated as input has overlap potential e.g. twooneight
+    line = line.replace('one','o1e').replace('two','t2o').replace('thr','3').replace('four','4').replace('fiv','5').replace('six','6').replace('seven','7').replace('igh','8').replace('nin','9')
+    
+    for char in line:
+        if char.isdigit():
+            numbers.append(char)
+    
+    if len( numbers ) != 1:
+        twoDigitNum = int(numbers[0] + numbers[-1])
+        count += twoDigitNum
+    else: #single entry so double it
+        twoDigitNum = int(numbers[0] + numbers[0])
+        count += twoDigitNum
+    print( numbers, origLine )   
+    
+
+
+
+print(f'Count is: ', count)
 
     
-#Find most calories
-sortedCals = sorted(elves, reverse=True)
-
-print(f"Most cals: {sortedCals[0]}" )
-sumCals = sum(sortedCals[0:3])
-print(f"Top 3 total cals: {sumCals}")  
-
 
